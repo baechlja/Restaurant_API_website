14028b3d941ab7e12fc9bfcbda9eb37193bd34ca
Jasmin Bächle <jasmin-baechle@web.de>
2021-11-21 16:33:55 +0100

    added a to do list

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const maps_url = "https://maps.googleapis.com/maps/api/distancematrix/json&key=xxx"
+//const maps_url = "https://maps.googleapis.com/maps/api/distancematrix/json&key=xxx"
+
+var origin1 = new google.maps.LatLng(55.930385, -3.118425);
+var origin2 = 'Greenwich, England';
+var destinationA = 'Stockholm, Sweden';
+var destinationB = new google.maps.LatLng(50.087692, 14.421150);
+
+var service = new google.maps.DistanceMatrixService();
+service.getDistanceMatrix(
+  {
+    origins: [origin1, origin2],
+    destinations: [destinationA, destinationB],
+    travelMode: 'DRIVING',
+    transitOptions: TransitOptions,
+    drivingOptions: DrivingOptions,
+    unitSystem: UnitSystem,
+    avoidHighways: Boolean,
+    avoidTolls: Boolean,
+  }, callback);
+
+function callback(response, status) {
+  // See Parsing the Results for
+  // the basics of a callback function.
+}
+
