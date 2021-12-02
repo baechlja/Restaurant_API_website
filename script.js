9d81dae447a925ea7a90538d27ca8165746dbafc
baechlja <73224461+baechlja@users.noreply.github.com>
2021-12-02 15:33:00 +0100

    optimized code

--- script.js
+++ script.js
@@ -2,16 +2,67 @@
 const maps_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
 //Konstanten von html dokument festlegen
 const lieferung = document.getElementById('lieferung')
-const checkbox_input_menus = document.getElementById('checkbox_lieferung') 
-const search_input_lieferadresse = document.getElementById('lieferadresse') 
+//Die Ausgewählten Menus erfassen
+const ordered_menus = [];
+if (document.getElementById("asian_check").checked) {
+  ordered_menus.push({
+    menu:   "Asian Fusion",
+    prep_time: 60,
+    price: 22
+})
+} else if (document.getElementById("arabic_check").checked) {
+  ordered_menus.push({
+    menu:   "Arabic Fusion",
+    prep_time: 50,
+    price: 18
+})
+} else if (document.getElementById("african_check").checked) {
+  ordered_menus.push({
+    menu:   "African Fusion",
+    prep_time: 40,
+    price: 20
+})
+} else if (document.getElementById("latin_check").checked) {
+  ordered_menus.push({
+    menu:   "Latin Fusion",
+    prep_time: 30,
+    price: 20
+})
+} else if (document.getElementById("other_check").checked) {
+  ordered_menus.push({
+    menu:   "Other Fusion",
+    prep_time: 20,
+    price: 21
+})
+} else if (document.getElementById("european_check").checked) {
+  ordered_menus.push({
+    menu:   "European Fusion",
+    prep_time: 70,
+    price: 15
+})
+}else {
+  ordered_menus.push({
+    menu:   "None",
+    prep_time: 0,
+    price: 0
+})
+}
+//Die eigegebene Lieferadresse speichern  
+const search_input_lieferadresse = document.getElementById('lieferadresse')
+//ToDo:Preis anhand der Menus berechnen 
 //const total_price_calculation = document.getElementsByClassName('Preis')
 const total_price_calculation = 50
+//Button zur Berechnung der Lieferzeit
 const calculate_button = document.getElementById('calculate_button')
+//Button zur Anzeige der Bestellung
 const view_button = document.getElementById('view_button')
 
 //Zubereitungszeit für die einzelnen Menüs festlegen
-const prepation_time = null 
+const preperation_time = null
+//Lieferzeit festlegen 
+const delivery_time = null
 
+//API laden
 async function loadData(api_key, destination, origin) {
   const url = `${maps_url}?key=${api_key}&destinations=${destination}&origins=${origin}`;
   const data = await fetch(url);
@@ -21,31 +72,22 @@ async function loadData(api_key, destination, origin) {
 }
 
 //calculate the total delivery_time
-async function calculate_total_delivery_time (delivery_time){
-  delivery_time = loadData("AIzaSyDf6dam-1N9aS0FMEmGf9-F1HZ057XhG5E","Rotzingen 45 79733 Görwihl BW, DE", "Hangstraße 46-50 Loerach, 79539 BW, DE");
+async function calculate_total_delivery_time (){
+  //delivery_time = loadData("enter your api key here","Rotzingen 45 79733 Görwihl BW, DE", "Hangstraße 46-50 Loerach, 79539 BW, DE");
+  const preperation_time = ordered_menus.reduce((sum, currentValue) => {
+    return sum + currentValue.prep_time;
+  }, 0);
   
-  if (checkbox_input_menus.value = "Asian Fusion") {
-    prepation_time = 60
-  } else if (checkbox_input_menus.value = "African Fusion") {
-    preperation_time = 40
-  } else if (checkbox_input_menus.value = "Arabic Fusion") {
-    preperation_time = 50
-  } else if (checkbox_input_menus.value = "European Fusion") {
-    preperation_time = 70
-  } else if (checkbox_input_menus.value = "Latin Fusion") {
-    preperation_time = 30
-  } else {
-    preperation_time = 20
-  }
-  const delivery_time_total = delivery_time + prepation_time
+  const delivery_time_total = delivery_time + preperation_time
   return delivery_time_total 
 }
 
 async function create_new_order(id,menus, price,adress, delivery_time_total) {
     const new_order = document.createElement('div');
-    new_order.classList.add('section_delivery');
+    new_order.classList.add('section-delivery');
 
     new_order.innerHTML = `
+    <div id="Bestellung">
     <div class="order">Bestellnummer: ${id}</div>
       <div class="order_info">
         <span>
@@ -55,28 +97,44 @@ async function create_new_order(id,menus, price,adress, delivery_time_total) {
           <span class="lieferzeit">Lieferzeit: ${delivery_time_total}</span>
         </span>
     </div>
+    </div>
   `;
   lieferung.appendChild(new_order);
 }
-
-async function create_total_time() {
+//eine Option erstellen die Bestellung ein- oder auszublenden
+function showOrHideDiv(div_id) {
+  var v = document.getElementById(div_id);
+  if (v.style.display === "none") {
+     v.style.display = "block";
+  } else {
+     v.style.display = "none";
+  }
+}
+async function create_total_time(delivery_time_total) {
   const total_time = document.createElement('div');
   total_time.classList.add('section_delivery');
 
     total_time.innerHTML = `
-    <div class="total_time">${delivery_time_total}</div>
+    <div class="total_time">Lieferzeit: ${delivery_time_total}</div>
   `;
   lieferung.appendChild(total_time);
 }
 
 function doSearch() {
     const order_id = Date.now().toString(36) + Math.random().toString(36);
-    const ordered_menus = checkbox_input_menus.value;
-    const total_price = total_price_calculation;
+    const order_menus = "Asian Fusion"//ordered_menus
+    const total_price =  50 //ordered_menus.reduce((sum, currentValue) => {
+     // return sum + currentValue.price;
+   // }, 0);
     const order_adress= search_input_lieferadresse.value;
-    const order_delivery_time = 45
-    create_new_order(order_id, ordered_menus, total_price, order_adress, order_delivery_time);
+    const order_delivery_time =  45 //calculate_total_delivery_time (delivery_time)
+    create_new_order(order_id, order_menus, total_price, order_adress, order_delivery_time);
   }
 
-calculate_button.addEventListener('click', create_total_time)
+function doCalculation() {
+  const delivery_time_total = calculate_total_delivery_time (delivery_time)
+  create_total_time(delivery_time_total)
+}
+
+calculate_button.addEventListener('click', doCalculation)
 view_button.addEventListener('click', doSearch);
