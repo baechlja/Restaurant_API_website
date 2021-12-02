24fe7af9a330e380899618cb05c7e93628e43250
baechlja <73224461+baechlja@users.noreply.github.com>
2021-12-02 23:02:00 +0100

    optimized code

--- script.js
+++ script.js
@@ -4,49 +4,49 @@ const maps_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
 const lieferung = document.getElementById('lieferung')
 //Die Ausgewählten Menus erfassen
 const ordered_menus = [];
-if (document.getElementById("asian_check").checked) {
-  ordered_menus.push({
-    menu:   "Asian Fusion",
-    prep_time: 60,
-    price: 22
-})
-} else if (document.getElementById("arabic_check").checked) {
-  ordered_menus.push({
-    menu:   "Arabic Fusion",
-    prep_time: 50,
-    price: 18
-})
-} else if (document.getElementById("african_check").checked) {
-  ordered_menus.push({
-    menu:   "African Fusion",
-    prep_time: 40,
-    price: 20
-})
-} else if (document.getElementById("latin_check").checked) {
-  ordered_menus.push({
-    menu:   "Latin Fusion",
-    prep_time: 30,
-    price: 20
-})
-} else if (document.getElementById("other_check").checked) {
-  ordered_menus.push({
-    menu:   "Other Fusion",
-    prep_time: 20,
-    price: 21
-})
-} else if (document.getElementById("european_check").checked) {
-  ordered_menus.push({
-    menu:   "European Fusion",
-    prep_time: 70,
-    price: 15
-})
-}else {
-  ordered_menus.push({
-    menu:   "None",
-    prep_time: 0,
-    price: 0
-})
+for (let i = 0; i < 6; i++) {
+  if (document.getElementById("asian_check").checked && ordered_menus.some(ordered_menus => ordered_menus.menu !== "Asian Fusion")) {
+    ordered_menus.push({
+      menu:   "Asian Fusion",
+      prep_time: 60,
+      price: 22
+  })
+  } else if (document.getElementById("arabic_check").checked && ordered_menus.some(ordered_menus => ordered_menus.menu !== "Arabic Fusion")) {
+    ordered_menus.push({
+      menu:   "Arabic Fusion",
+      prep_time: 50,
+      price: 18
+  })
+  } else if (document.getElementById("african_check").checked && ordered_menus.some(ordered_menus => ordered_menus.menu !== "African Fusion")) {
+    ordered_menus.push({
+      menu:   "African Fusion",
+      prep_time: 40,
+      price: 20
+  })
+  } else if (document.getElementById("latin_check").checked && ordered_menus.some(ordered_menus => ordered_menus.menu !== "Latin Fusion")) {
+    ordered_menus.push({
+      menu:   "Latin Fusion",
+      prep_time: 30,
+      price: 20
+  })
+  } else if (document.getElementById("other_check").checked && ordered_menus.some(ordered_menus => ordered_menus.menu !== "Other Fusion")) {
+    ordered_menus.push({
+      menu:   "Other Fusion",
+      prep_time: 20,
+      price: 21
+  })
+  } else if (document.getElementById("european_check").checked && ordered_menus.some(ordered_menus => ordered_menus.menu !== "European Fusion")) {
+    ordered_menus.push({
+      menu:   "European Fusion",
+      prep_time: 70,
+      price: 15
+  })
+  }else {
+    ordered_menus.push(" ")
+  }
 }
+
+
 //Die eigegebene Lieferadresse speichern  
 const search_input_lieferadresse = document.getElementById('lieferadresse')
 //ToDo:Preis anhand der Menus berechnen 
@@ -84,11 +84,11 @@ async function calculate_total_delivery_time (){
 
 async function create_new_order(id,menus, price,adress, delivery_time_total) {
     const new_order = document.createElement('div');
-    new_order.classList.add('section-delivery');
+    new_order.classList.add('bestellansicht');
 
     new_order.innerHTML = `
-    <div id="Bestellung">
-    <div class="order">Bestellnummer: ${id}</div>
+    <div class = "orders">
+    <div class="order_id">Bestellnummer: ${id}</div>
       <div class="order_info">
         <span>
           <span class="menus">Menus: ${menus}</span>
@@ -98,6 +98,7 @@ async function create_new_order(id,menus, price,adress, delivery_time_total) {
         </span>
     </div>
     </div>
+    
   `;
   lieferung.appendChild(new_order);
 }
@@ -122,14 +123,14 @@ async function create_total_time(delivery_time_total) {
 
 function doSearch() {
     const order_id = Date.now().toString(36) + Math.random().toString(36);
-    const order_menus = "Asian Fusion"//ordered_menus
-    const total_price =  50 //ordered_menus.reduce((sum, currentValue) => {
-     // return sum + currentValue.price;
-   // }, 0);
+    const order_menus = JSON.stringify(ordered_menus, null, 2)
+    const total_price =  ordered_menus.reduce((sum, currentValue) => {
+     return sum + currentValue.price;
+   }, 0);
     const order_adress= search_input_lieferadresse.value;
-    const order_delivery_time =  45 //calculate_total_delivery_time (delivery_time)
+    const order_delivery_time =  calculate_total_delivery_time (delivery_time)
     create_new_order(order_id, order_menus, total_price, order_adress, order_delivery_time);
-  }
+  };
 
 function doCalculation() {
   const delivery_time_total = calculate_total_delivery_time (delivery_time)
